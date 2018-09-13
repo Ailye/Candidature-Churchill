c9f99b5f17f45377a8e3ec7dd0adb3fd983cbf32
P-Hugo <ph.meillot@gmail.com>
2018-09-13 18:02:22 +0200

    ajout du js, devellopement du form

---
+++ page3script.js
@@ -0,0 +1,19 @@
+'use script'
+let txt = document.getElementById(jobdep);
+$("#selector").on("click", function(event){
+		console.log("CA MARCHE");
+		txt = "Dear World dictator";
+		console.log(event);
+
+	});
+$("#selector").selectedIndex
+console.log($("#selector"))
+
+
+switch(indexselected)
+{
+	case 1 : function addtext(text) {document.form.textarea.value = document.form.textarea.value+= text;} ("Dear World dictator")
+	case 2 : document.write("Ô Imperator")
+	case 3 : document.write("Glorious painter")
+	case 4 : document.write("Hey, sexy")
+}
